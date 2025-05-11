6a3fe1a570c7685da99bde8be695bbc2c5b22d1e
Sarafanov Nikita <52919064+niksanhts@users.noreply.github.com>
2025-05-11 09:12:54 +0300

    Done - Lab6; Start - lab7

--- Assets/Lab6/SceneLoader.cs
+++ Assets/Lab6/SceneLoader.cs
@@ -3,16 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Название сцены для загрузки (укажите в инспекторе)
     public string sceneNameToLoad;
 
-    // Метод для загрузки сцены (вызывается при нажатии кнопки)
     public void LoadTargetScene()
     {
-        // Проверяем, что название сцены не пустое
         if (!string.IsNullOrEmpty(sceneNameToLoad))
         {
-            // Загружаем указанную сцену
             SceneManager.LoadScene(sceneNameToLoad);
         }
         else
@@ -21,18 +17,15 @@ public class SceneLoader : MonoBehaviour
         }
     }
 
-    // Метод для перезагрузки текущей сцены
     public void ReloadCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    // Метод для выхода из игры (работает в билде)
     public void QuitGame()
     {
         Application.Quit();
         
-        // Для теста в редакторе
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
